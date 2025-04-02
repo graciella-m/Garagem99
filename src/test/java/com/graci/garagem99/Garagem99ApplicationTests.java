2ce88d28a13245d831c809dfeb18441ec205bef1
graciella-m <graciellamaiamachado@gmail.com>
2025-04-02 11:13:37 -0300

    Criei o projeto; Importei para IDE; Iniciei as estruturas dos Packages!

---
+++ src/test/java/com/graci/garagem99/Garagem99ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graci.garagem99;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Garagem99ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
